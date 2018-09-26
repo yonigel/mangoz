ba219a1c4397da30b6bf80fd2992362bb1e08c4f
yonatang <yonatan.gelbard@comm-it.com>
2018-09-26 17:20:08 +0300

    basic room for players

--- server/controllers/socket.controller.js
+++ server/controllers/socket.controller.js
@@ -12,11 +12,33 @@ function initSocket(http) {
     io.on('connection', (socket) => {
         console.log('a user connected');
 
-        io.on('disconnect', () => {
+        setUserIntoRoom(socket, 'mangozRoom');
+
+        socket.on('disconnect', () => {
             console.log(`user disconnected`);
         });
-        
-    });
 
-    
+    });
+}
+
+function getNumberOfUsersOfRoom(roomName) {
+    if (io.sockets.adapter.rooms[roomName] != undefined)
+        return io.sockets.adapter.rooms[roomName].length;
+    else return 0;
+}
+
+function getUsersFromRoom(roomName) {
+    return io.sockets.adapter.rooms[roomName];
+}
+
+function setUserIntoRoom(socket, roomName) {
+    if(getNumberOfUsersOfRoom(roomName) < 4) {
+        socket.join(roomName, () => {
+            io.to(roomName).emit('new user got into room1');
+            console.log(`room ${roomName} has ${getNumberOfUsersOfRoom(roomName)} players`)
+        });
+    }
+    else {
+
+    }
 }
